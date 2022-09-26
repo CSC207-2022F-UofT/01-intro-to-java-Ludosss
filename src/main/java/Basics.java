4a02e75b3a94ca9b4497c100dc02b7f02fae9466
Xavier Ibanez-Padron <xavier.ibanezpadron@mail.utoronto.ca>
2022-09-25 22:34:31 -0400

    Finished onsum method

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -179,6 +179,10 @@ public class Basics {
          *                (e.g. arr[i] gives you the item at index i).
          */
 
+        for (int i=1;i< arr.length;i+=2) {
+            current_sum += arr[i];
+        }
+
         return current_sum;
     }
 
